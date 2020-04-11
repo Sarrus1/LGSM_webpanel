ca96af953460c35192f7eedad68d143dfc39d9ba
omegasix0611@gmail.com <Charles@charless-imac.home>
2020-04-11 18:08:29 +0200

    no more hardcoded

--- webpanel/views.py
+++ webpanel/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+servers = [
+    {
+        'Name': 'AWP_only',
+        'IP': '192.168.1.107',
+        'Port': '27015',
+        'Status': 'Running'
+    },
+    {
+        'Name': 'Surf',
+        'IP': '192.168.1.107',
+        'Port': '27015',
+        'Status': 'Running'
+    }
+]
+
+
 def index(request):
-    return render(request, 'webpanel/index.html')
+    context= {
+        'servers': servers
+    }
+    return render(request, 'webpanel/index.html', context)
 
 def buttons(request):
     return render(request, 'webpanel/buttons.html')
